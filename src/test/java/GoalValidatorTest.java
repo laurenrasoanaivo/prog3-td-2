09cb65c135dd30065530b10f96ca5cf2dbf9adfc
laurenrasoanaivo <hei.lauren.3@gmail.com>
2023-01-24 13:34:21 +0300

    feat: add many tests

--- src/test/java/GoalValidatorTest.java
+++ src/test/java/GoalValidatorTest.java
@@ -1,5 +1,8 @@
+import app.foot.controller.rest.Player;
+import app.foot.controller.rest.PlayerScorer;
 import app.foot.controller.validator.GoalValidator;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 //TODO-1: complete these tests
 public class GoalValidatorTest {
@@ -7,26 +10,80 @@ public class GoalValidatorTest {
 
     @Test
     void accept_ok() {
+        Player player = Player.builder()
+                .id(1)
+                .name("Paul")
+                .isGuardian(false)
+                .build();
+        PlayerScorer playerScorer = PlayerScorer.builder()
+                .player(player)
+                .scoreTime(40)
+                .isOG(false)
+                .build();
+        assertDoesNotThrow(() -> subject.accept(playerScorer));
     }
 
     //Mandatory attributes not provided : scoreTime
     @Test
     void accept_ko() {
-
+        Player player = Player.builder()
+                .id(1)
+                .name("Paul")
+                .isGuardian(false)
+                .build();
+        PlayerScorer playerScorer = PlayerScorer.builder()
+                .player(player)
+                .scoreTime(-40)
+                .isOG(false)
+                .build();
+        assertThrows(Exception.class, () -> subject.accept(playerScorer));
     }
 
     @Test
     void when_guardian_throws_exception() {
-
+        Player player = Player.builder()
+                .id(1)
+                .name("Paul")
+                .isGuardian(true)
+                .build();
+        PlayerScorer playerScorer = PlayerScorer.builder()
+                .player(player)
+                .scoreTime(15)
+                .isOG(false)
+                .build();
+        Exception exception = assertThrows(Exception.class, () -> subject.accept(playerScorer));
+        assertEquals("Player#"+player.getId()+" is a guardian so they cannot score.", exception.getMessage());
     }
 
     @Test
     void when_score_time_greater_than_90_throws_exception() {
-
+        Player player = Player.builder()
+                .id(1)
+                .name("Paul")
+                .isGuardian(false)
+                .build();
+        PlayerScorer playerScorer = PlayerScorer.builder()
+                .player(player)
+                .scoreTime(91)
+                .isOG(false)
+                .build();
+        Exception exception = assertThrows(Exception.class, () -> subject.accept(playerScorer));
+        assertEquals("Player#"+player.getName()+" cannot score before after minute 90.", exception.getMessage());
     }
 
     @Test
     void when_score_time_less_than_0_throws_exception() {
-
+        Player player = Player.builder()
+                .id(1)
+                .name("Paul")
+                .isGuardian(false)
+                .build();
+        PlayerScorer playerScorer = PlayerScorer.builder()
+                .player(player)
+                .scoreTime(-1)
+                .isOG(false)
+                .build();
+        Exception exception = assertThrows(Exception.class, () -> subject.accept(playerScorer));
+        assertEquals("Player#"+player.getId()+" cannot score before before minute 0.", exception.getMessage());
     }
 }
--- src/test/java/PlayerMapperTest.java
+++ src/test/java/PlayerMapperTest.java
@@ -1,19 +1,102 @@
+import app.foot.model.Player;
+import app.foot.model.PlayerScorer;
+import app.foot.repository.MatchRepository;
+import app.foot.repository.PlayerRepository;
+import app.foot.repository.entity.MatchEntity;
+import app.foot.repository.entity.PlayerEntity;
+import app.foot.repository.entity.PlayerScoreEntity;
+import app.foot.repository.entity.TeamEntity;
+import app.foot.repository.mapper.PlayerMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 //TODO-2: complete these tests
+@Slf4j
 public class PlayerMapperTest {
+    MatchRepository matchRepository = mock(MatchRepository.class);
+    PlayerRepository playerRepository = mock(PlayerRepository.class);
+    PlayerMapper subject = new PlayerMapper(matchRepository, playerRepository);
+
     @Test
     void player_to_domain_ok() {
-
+        TeamEntity teamEntity = TeamEntity.builder()
+                .id(1)
+                .name("Ghana")
+                .build();
+        PlayerEntity playerEntity = PlayerEntity.builder()
+                .id(1)
+                .name("Paul")
+                .guardian(false)
+                .team(teamEntity)
+                .build();
+        Player expected = Player.builder()
+                .id(1)
+                .name("Paul")
+                .isGuardian(false)
+                .teamName(teamEntity.getName())
+                .build();
+        Player actual = subject.toDomain(playerEntity);
+        assertEquals(expected, actual);
     }
 
     @Test
     void player_scorer_to_domain_ok() {
-
+        PlayerScorer expected = PlayerScorer.builder()
+                .isOwnGoal(false)
+                .minute(13)
+                .player(subject.toDomain(playerEntity))
+                .build();
+        PlayerScorer actual = subject.toDomain(playerScoreEntity);
+        assertEquals(expected, actual);
     }
 
     @Test
     void player_scorer_to_entity_ok() {
-
+        when(matchRepository.findById(1)).thenReturn(Optional.of(matchEntity));
+        when(playerRepository.findById(1)).thenReturn(Optional.of(playerEntity));
+        PlayerScorer playerScorer = PlayerScorer.builder()
+                .player(subject.toDomain(playerEntity))
+                .minute(13)
+                .isOwnGoal(false)
+                .build();
+        PlayerScoreEntity actual = subject.toEntity(matchEntity.getId(), playerScorer);
+        assertEquals(playerScoreEntity, actual);
     }
+
+    TeamEntity teamBarea = TeamEntity.builder()
+            .id(1)
+            .name("Barea")
+            .build();
+    TeamEntity teamGhana = TeamEntity.builder()
+            .id(1)
+            .name("Ghana")
+            .build();
+    MatchEntity matchEntity = MatchEntity.builder()
+            .id(1)
+            .teamA(teamBarea)
+            .teamB(teamGhana)
+            .scorers(List.of())
+            .stadium("stad")
+            .build();
+    PlayerEntity playerEntity = PlayerEntity.builder()
+            .id(1)
+            .name("Paul")
+            .guardian(false)
+            .team(teamBarea)
+            .build();
+
+    PlayerScoreEntity playerScoreEntity = PlayerScoreEntity.builder()
+            .match(matchEntity)
+            .player(playerEntity)
+            .minute(13)
+            .ownGoal(false)
+            .build();
+
 }
